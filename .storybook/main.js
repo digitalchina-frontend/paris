57cddf7d87b77326d7a756a6cb0494b0c3caed86
fancn21th <fancn21th@gmail.com>
2022-04-30 16:57:14 +0800

    import vue com into sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,28 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue',
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' },
+        {
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true,
+          },
+        },
+      ],
+    })
+
+    // Return the altered config
+    return config
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,23 @@
+import Vue from 'vue'
+// 手动注册 各种 antd 组件
+import { Form, Input, Select, Button, Card, Row, Col, Table, Icon } from 'ant-design-vue'
+// 手动引入 antd global.less
+import '../src/global.less'
+
+Vue.component(Form.name, Form)
+Vue.component(Form.Item.name, Form.Item)
+Vue.component(Input.name, Input)
+Vue.component(Select.name, Select)
+Vue.component(Select.Option.name, Select.Option)
+Vue.component(Button.name, Button)
+Vue.component(Card.name, Card)
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Table.name, Table)
+Vue.component(Icon.name, Icon)
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
