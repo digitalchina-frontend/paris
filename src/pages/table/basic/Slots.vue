e6a2af9287e4c2cd853dccdafcb0ce18dbae53ac
fancn21th <fancn21th@gmail.com>
2022-05-01 16:42:17 +0800

    引入 DcTable

--- src/pages/table/basic/Slots.vue
+++ src/pages/table/basic/Slots.vue
@@ -1,9 +1,71 @@
 <template>
-  <h2>Hello DcForm With Slots</h2>
+  <dc-table :data="data" :columns="columns"></dc-table>
 </template>
 
 <script>
-export default {}
+import DcTable from '@/components/_DC/Table'
+
+export default {
+  components: {
+    DcTable,
+  },
+  data() {
+    return {
+      columns: [
+        {
+          dataIndex: 'name',
+          key: 'name',
+          slots: { title: 'customTitle' },
+          scopedSlots: { customRender: 'name' },
+        },
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+        },
+        {
+          title: 'Tags',
+          key: 'tags',
+          dataIndex: 'tags',
+          scopedSlots: { customRender: 'tags' },
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          scopedSlots: { customRender: 'action' },
+        },
+      ],
+      data: [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style></style>
