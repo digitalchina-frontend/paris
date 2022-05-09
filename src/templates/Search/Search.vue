bca0cdd511246d94da4846dba4df0b06f8e63b55
fancn21th <fancn21th@gmail.com>
2022-05-09 16:48:37 +0800

    jsx 插槽

--- src/templates/Search/Search.vue
+++ src/templates/Search/Search.vue
@@ -2,9 +2,26 @@
 import DcTable from '@/components/_DC/Table'
 import Renderless from './Search.renderless.vue'
 
+// jsx 参考: https://github.com/vuejs/jsx-vue2
 export default {
   functional: true,
   // JSX 语法
-  render: (h, { props }) => <Renderless render={() => <DcTable dataSource={props.data} columns={props.columns} />} />,
+  render: (h, { props }) => {
+    const scopedSlots = {
+      name: (text) => <a>{text}</a>,
+      customTitle: () => <a-icon type="smile-o" />,
+    }
+    return (
+      <Renderless
+        render={() => (
+          <DcTable dataSource={props.data} columns={props.columns} scopedSlots={scopedSlots}>
+            <template slot="customTitle">
+              <a-icon type="smile-o" /> Name
+            </template>
+          </DcTable>
+        )}
+      />
+    )
+  },
 }
 </script>
