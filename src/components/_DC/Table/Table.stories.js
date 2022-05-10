2a5ab9185d3948989450ec1333bb2f7b122bca4b
fancn21th <fancn21th@gmail.com>
2022-05-10 16:47:08 +0800

    storybook 部分自定义兼容

--- src/components/_DC/Table/Table.stories.js
+++ src/components/_DC/Table/Table.stories.js
@@ -19,54 +19,54 @@ export const Primary = Template.bind({})
 Primary.args = {
   columns: [
     {
-      dataIndex: 'name',
-      key: 'name',
-      slots: { title: 'customTitle' },
-      scopedSlots: { customRender: 'name' },
+      dataIndex: 'textField',
+      key: 'textField',
+      title: 'Text',
     },
+
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      dataIndex: 'customField',
+      key: 'customField',
+      title: 'Custom',
+      customRender: (text) => `爱好: ${text}`,
     },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      scopedSlots: { customRender: 'tags' },
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      scopedSlots: { customRender: 'action' },
-    },
+    // {
+    //   title: 'Action',
+    //   // _ aka text 字段 为空
+    //   customRender: (_, record) => (
+    //     <div>
+    //       <a vOn:click={() => this.actionHandler(record)}>{record.textField}</a>
+    //       <a-divider type="vertical" />
+    //       <a vOn:click={() => this.actionHandler(record)}>{record.customField}</a>
+    //       <a-divider type="vertical" />
+    //       <a class="ant-dropdown-link">
+    //         {' '}
+    //         更多操作 <a-icon type="down" />{' '}
+    //       </a>
+    //     </div>
+    //   ),
+    // },
   ],
   dataSource: [
     {
+      key: '0',
+      textField: '唐僧',
+      customField: '唠叨',
+    },
+    {
       key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
+      textField: '八戒',
+      customField: '贪婪',
     },
     {
       key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
+      textField: '悟空',
+      customField: '较真',
     },
     {
       key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
+      textField: '沙僧',
+      customField: '勤勉',
     },
   ],
 }
