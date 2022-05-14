66af6a99ac5f9865613f5e209aa8e21875c9bf6d
fancn21th <fancn21th@gmail.com>
2022-05-14 13:54:20 +0800

    hoc 更新

--- src/pages/hoc/BasicHoc.vue
+++ src/pages/hoc/BasicHoc.vue
@@ -3,46 +3,23 @@
   首先 选择 HOC 的目的是什么？
 
     1. 代码复用
+      将一个页面的代码分成2个部分
       1.1 一部分代码是模板可以复用
-      1.2 一部分代码是可配置的
+      1.2 一部分代码是业务配置 随业务变化
 
-    代码参考：
+    hoc 参考：
       * https://segmentfault.com/p/1210000012743259/read
       * https://medium.com/bethink-pl/higher-order-components-in-vue-js-a79951ac9176
 */
-import Vue from 'vue'
-import Foo from './Foo.vue'
-import { getList } from '@/services/xhr/demo'
 
-const hoc = (component) => {
-  return Vue.component('withHoc', {
-    props: component.props,
-    data() {
-      return {
-        tableData: [],
-      }
-    },
-    methods: {},
-    created() {
-      getList({}).then((res) => {
-        this.tableData = res.result.data
-      })
-    },
-    mounted() {},
-    beforeDestroy() {},
-    render(createElement) {
-      return createElement(component, {
-        props: { ...this.$props, data: this.tableData },
-        on: this.$listeners,
-      })
-    },
-  })
-}
+import Render from './Render.vue'
+import Renderless from './Renderless'
 
 export default {
   functional: true,
   render(h, { props }) {
-    return h(hoc(Foo), { props: props.default.data() })
+    // 这里的实现依赖于 路由组件的接口设计 https://v3.router.vuejs.org/zh/guide/essentials/passing-props.html#%E5%AF%B9%E8%B1%A1%E6%A8%A1%E5%BC%8F
+    return h(Renderless(Render), { props: props.default.data() })
   },
 }
 </script>
--- src/pages/hoc/Render.vue
+++ src/pages/hoc/Render.vue
@@ -7,10 +7,26 @@
 </template>
 
 <script>
+/*
+  渲染组件:
+    1. 接受 props
+    2. 设计内部状态
+    3. 设计派生状态
+    4. 处理渲染
+*/
 import DcTable from '@/components/_DC/Table'
 
 export default {
-  props: ['columns', 'data'],
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
   components: {
     DcTable,
   },
