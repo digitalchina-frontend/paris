85313e38b983bde1e722d1bd6452ba6742198cbb
fancn21th <fancn21th@gmail.com>
2022-06-30 18:47:50 +0800

    数据联通

--- src/views/example/Foo.vue
+++ src/views/example/Foo.vue
@@ -1,9 +1,59 @@
 <template>
-  <h2>Hello Foo</h2>
+  <page-header-wrapper>
+    <a-card :bordered="false">
+      <a-table :columns="columns" :data-source="data"> </a-table>
+    </a-card>
+  </page-header-wrapper>
 </template>
 
 <script>
-export default {}
+import { getList } from '@/services/xhr/demo'
+
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+]
+
+export default {
+  data() {
+    return {
+      columns,
+      data: [],
+    }
+  },
+  async created() {
+    // 嵌套式的结构语法
+    const {
+      result: { data },
+    } = await getList()
+    this.data = data
+  },
+}
 </script>
 
 <style>
