bdcb00cf22249484c351af71f9ba2078d7538dfe
fancn21th <fancn21th@gmail.com>
2022-07-01 09:11:50 +0800

    remove console.log

--- src/mock/services/user.js
+++ src/mock/services/user.js
@@ -1,8 +1,8 @@
 import Mock from 'mockjs2'
 import { builder } from '../util'
 
-const info = options => {
-  console.log('options', options)
+const info = (options) => {
+  // console.log('options', options)
   const userInfo = {
     id: '4291d7da9005377ec9aec4a71ea837f',
     name: '天野远子',
@@ -472,7 +472,7 @@ const info = options => {
  * @param {*} options
  * @returns
  */
-const userNav = options => {
+const userNav = (options) => {
   const nav = [
     // dashboard
     {
